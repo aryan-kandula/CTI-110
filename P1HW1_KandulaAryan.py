c85fa66c839c765fb2d5b67ea4eb90372f263bad
aryan-kandula <kandulaa1847@student.faytechcc.edu>
2024-11-26 09:45:00 -0500

    Add files via upload

---
+++ P1HW1_KandulaAryan.py
@@ -0,0 +1,21 @@
+# Aryan Kandula
+# 11/14/2024
+# P1HW1
+# Basic Calculation
+print('-----Calculating Exponents-----')
+print()
+base= int(input('Enter an integer as the base value: '))
+exponent= int(input('Enter an integer as the exponent: '))
+result= pow(base,exponent)
+print()
+print( base, 'raised to the power of' ,exponent, 'is' ,result, '!!')
+print()
+print()
+print('-----Addition and Subtraction----')
+print()
+int_start= int(input('Enter a starting integer: '))
+int_add= int(input('Enter an integer to add: '))
+int_substract= int(input('Enter an integer to subtract: '))
+final_result= (int_start + int_add - int_substract)
+print()
+print(int_start, '+' ,int_add, '-' ,int_substract, 'is equal to' ,final_result)
